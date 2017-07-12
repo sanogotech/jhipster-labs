b2f75c7735ab4263b33fecc35b8be91cb59b7ce5
James Gaglo <jgaglo@peopleinput.com>
2017-07-12 19:25:37 +0000

    Initial commit

---
+++ gateway/src/main/java/com/mygglo/jhipster/gateway/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.mygglo.jhipster.gateway.security;
---
+++ gateway/src/main/java/com/mygglo/jhipster/gateway/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.mygglo.jhipster.gateway.web.rest;
